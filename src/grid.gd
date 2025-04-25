2460be867d6fbe2a0ded2b8147768e9c47aca045
MichaelW6729 <michaelwall10@icloud.com>
2025-04-24 22:47:29 -0400

    Add files via upload

--- src/grid.gd
+++ src/grid.gd
@@ -8,7 +8,8 @@ var x_start := 148
 var y_start := 92
 var offset := 100
 var four_piece_chance := 25
-
+var x_range = []
+var y_range = []
 
 @export var two_piece : PackedScene
 @export var four_piece : PackedScene
@@ -34,8 +35,7 @@ func grid_to_pixel(grid_pos: Vector2) -> Vector2:
 	var tile_spacing = 10
 	var grid_size = 4
 	var total_size = grid_size * tile_size + (grid_size - 1) * tile_spacing
-
-	# Get top-left starting point to center the grid in the parent Node2D
+	
 	var start_x = (500 - total_size) / 2
 	var start_y = (600 - total_size) / 2
 
@@ -67,13 +67,74 @@ func is_blank_space():
 	return false
 #The functions above help with getting the grid visible
 #While all functions in this code were inspired from many youtube videos,
-#the section of code above is in no way mine
+#the section of code above is in no way mine (except the array and ready functions)
 #I STOLE THESE FROM MULTIPLE YOUTUBE TUTORIALS AND ADJUSTED THEM FOR MY GRID
 #THEY ARE NOT MY ORIGINAL CODE, I REPEAT THEY ARE NOT MINE
 
+func combine_pieces():
+	var combine = []
+	for i in range(width):
+		combine.append([])
+		for j in range(height):
+			combine[i].append([])
+	return combine
+
+
 func move_pieces(direction: Vector2):
-	#if Input.action_press("ui_left"):
-		pass
+	var merge = []
+	var combined = combine_pieces()
+	var return_false = false
+	var x_piece = 0
+	var y_piece = 0
+	var check_position = Vector2(0,0)
+	if direction.x == 1:
+		x_range = [0, 1, 2, 3]
+		y_range = [0, 1, 2, 3]
+		x_piece = 1
+		y_piece = 1
+	elif direction.x == -1:
+		x_range = [3, 2, 1, 0]
+		y_range = [0, 1, 2, 3]
+		x_piece = -1
+		y_piece = 1
+	elif direction.y == 1:
+		x_range = [0, 1, 2, 3]
+		y_range = [0, 1, 2, 3]
+		x_piece = 1
+		y_piece = 1
+	elif direction.y == -1:
+		x_range = [0, 1 ,2, 3]
+		y_range = [3, 2, 1, 0]
+		x_piece = 1
+		y_piece = -1
+	for i in x_range:
+		for j in y_range:
+			var pos = Vector2(i,j)
+			var tile = board[i][j]
+			
+			if tile == null:
+				continue
+			var current_position = Vector2(i,j)
+			check_position = pos + Vector2(x_piece, y_piece)
+			
+			while is_in_grid(check_position):
+				var next_tile = board[check_position.x][check_position.y]
+				
+				if next_tile == null:
+					board[check_position.x][check_position.y] = board[current_position.x][current_position.y]
+					board[pos.x][pos.y] = null
+					board[check_position.x][check_position.y].position = grid_to_pixel(check_position)
+					continue
+				elif next_tile == tile.value and combined[check_position.x][check_position.y] == false:
+					var new_tile = tile.next_value.instantiate()
+					add_child(new_tile)
+					board[current_position.x][current_position.y] = null
+					new_tile.position = grid_to_pixel(check_position)
+					break
+				
+				
+	
+
 	
 
 
